1fcca916b93170fe490bafa04b1c927f9a5bba77
lc-clo <2953527928@qq.com>
2022-12-21 20:40:00 +0800

    master

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -1,38 +1,33 @@
 <template>
-  <div>
-    <h1>App 根组件</h1>
+  <div class="left-container">
+    <h3>Left 组件</h3>
+    <button @click="getInfo">发起 GET 请求</button>
     <button @click="btnGetBooks">获取图书列表的数据</button>
-
-    <hr />
-
-    <div class="box">
-      <Left></Left>
-      <Right></Right>
-    </div>
   </div>
 </template>
 
 <script>
-import Left from '@/components/Left.vue'
-import Right from '@/components/Right.vue'
+// import axios from 'axios'
 
 export default {
   methods: {
+    async getInfo() {
+      const { data: res } = await this.$http.get('/api/get')
+      console.log(res)
+    },
     // 点击按钮，获取图书列表的数据
     async btnGetBooks() {
       const { data: res } = await this.$http.get('/api/getbooks')
       console.log(res)
     }
-  },
-  components: {
-    Left,
-    Right
   }
 }
 </script>
 
 <style lang="less" scoped>
-.box {
-  display: flex;
+.left-container {
+  background-color: orange;
+  min-height: 200px;
+  flex: 1;
 }
 </style>
--- src/components/Right.vue
+++ src/components/Right.vue
@@ -1,38 +1,33 @@
 <template>
-  <div>
-    <h1>App 根组件</h1>
+  <div class="right-container">
+    <h3>Right 组件</h3>
+    <button @click="postInfo">发起 POST 请求</button>
     <button @click="btnGetBooks">获取图书列表的数据</button>
-
-    <hr />
-
-    <div class="box">
-      <Left></Left>
-      <Right></Right>
-    </div>
   </div>
 </template>
 
 <script>
-import Left from '@/components/Left.vue'
-import Right from '@/components/Right.vue'
+// import axios from 'axios'
 
 export default {
   methods: {
+    async postInfo() {
+      const { data: res } = await this.$http.post('/api/post', { name: 'zs', age: 20 })
+      console.log(res)
+    },
     // 点击按钮，获取图书列表的数据
     async btnGetBooks() {
       const { data: res } = await this.$http.get('/api/getbooks')
       console.log(res)
     }
-  },
-  components: {
-    Left,
-    Right
   }
 }
 </script>
 
 <style lang="less" scoped>
-.box {
-  display: flex;
+.right-container {
+  background-color: skyblue;
+  min-height: 200px;
+  flex: 1;
 }
 </style>
